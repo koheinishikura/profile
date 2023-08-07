01d258197156401def9662b4ca4f5f09ab4ec982
KOHEI NISHIKURA <nissiee.249ra@gmail.com>
2023-08-08 00:59:10 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,130 +1,144 @@
+// ページのロード完了時に実行
 window.onload = function() {
   // スクロールを禁止にする関数
   function disableScroll(event) {
-    event.preventDefault();
+    event.preventDefault(); // デフォルトのタッチイベントを無効にする
   }
 
-  // スクロール禁止
+  // スクロール禁止機能
   document.getElementById('on').onclick = function() {
-    // イベントと関数を紐付け
+    // タッチ移動イベントとスクロール禁止関数を紐付け
     document.addEventListener('touchmove', disableScroll, { passive: false });
-    document.body.classList.add('overflow-hidden');
+    document.body.classList.add('overflow-hidden'); // オーバーフローを隠すCSSクラスを追加
   }
 
-  // スクロール解除
+  // スクロール解除機能
   document.getElementById('off').onclick = function() {
-    // イベントと関数を紐付け
+    // タッチ移動イベントとスクロール禁止関数の紐付けを解除
     document.removeEventListener('touchmove', disableScroll, { passive: false });
-    document.body.classList.remove('overflow-hidden');
+    document.body.classList.remove('overflow-hidden'); // オーバーフローを隠すCSSクラスを削除
   }
-  // 現在のブラウザサイズに合わせてスライドショーのサイズを調整
+
+  // スライドショーのサイズを調整する関数
   function adjustSlideshowSize() {
     var slideshowContainer = document.querySelector(".sp-slideshow");
     if (slideshowContainer) {
       var width = window.innerWidth;
-      // 縦横比を16:9に保つ
-      var height = (width * 9) / 16;
-
-      // ここでスライドショーのサイズを調整できます。例：
+      var height = (width * 9) / 16; // 縦横比を16:9に保つ
       slideshowContainer.style.width = width + 'px';
       slideshowContainer.style.height = height + 'px';
     }
   }
 
-    // ブラウザのサイズを読み取り、スライドショーのサイズを調整
+  // ブラウザのサイズを読み取り、スライドショーのサイズを調整
+  adjustSlideshowSize();
+
+  // ブラウザリサイズ時にスライドショーのサイズを調整
+  window.addEventListener('resize', adjustSlideshowSize);
+
+  // スライドショーの表示/非表示を切り替える関数
+  function handleSlideshowVisibility() {
+    var slideshowContainer = document.querySelector('.sp-slideshow');
+    if (slideshowContainer) {
+      if (window.innerWidth <= 640) {
+        slideshowContainer.style.display = 'block';
+      } else {
+        slideshowContainer.style.display = 'none';
+      }
+    }
+  }
+
+  // 初期ロード時に関数を実行
+  handleSlideshowVisibility();
+
+  // ブラウザリサイズ時にスライドショーのサイズを調整
+  window.addEventListener('resize', function() {
     adjustSlideshowSize();
+    handleSlideshowVisibility(); // リサイズ時に表示を切り替え
+  });
 }
 
-// ピンチイン・ピンチアウト禁止
+// ピンチイン・ピンチアウトを禁止する機能
 document.documentElement.addEventListener('touchstart', function (e) {
   if (e.touches.length >= 2) {e.preventDefault();}
 }, {passive: false});
 
+// テキストをクリップボードにコピーする関数
 function copyToClipboard(text){
-// テキストコピー用の一時要素を作成
-const pre = document.createElement('pre');
-
-// テキストを選択可能にしてテキストセット
-pre.style.webkitUserSelect = 'auto';
-pre.style.userSelect = 'auto';
-pre.textContent = text;
-
-// 要素を追加、選択してクリップボードにコピー
-document.body.appendChild(pre);
-document.getSelection().selectAllChildren(pre);
-const result = document.execCommand('copy');
-
-// 要素を削除
-document.body.removeChild(pre);
-
-return result;
+  const pre = document.createElement('pre');
+  pre.style.webkitUserSelect = 'auto';
+  pre.style.userSelect = 'auto';
+  pre.textContent = text;
+  document.body.appendChild(pre);
+  document.getSelection().selectAllChildren(pre);
+  const result = document.execCommand('copy');
+  document.body.removeChild(pre); // 一時要素を削除
+  return result;
 }
 
-// DOMのロード完了時に実行
+// DOMのロード完了時にスライドショーを制御する機能
 document.addEventListener("DOMContentLoaded", function() {
-  // スライドの要素をすべて取得
-  var slides = document.querySelectorAll(".sp-slide");
-  // 現在のスライドのインデックス
-  var currentSlide = 0;
+  var slides = document.querySelectorAll(".sp-slide"); // スライド要素をすべて取得
+  var currentSlide = 0; // 現在のスライドのインデックス
 
-// 指定されたインデックスのスライドを表示
-function showSlide(slideIndex) {
-  // すべてのスライドを非表示に設定
-  slides.forEach(function(slide) {
-    slide.style.display = "none"; // 非表示に設定
-    slide.style.opacity = 0; // 透明度を0に設定
-  });
-  // 指定されたスライドを表示
-  slides[slideIndex].style.display = "block";
-  // 少し遅延させてから透明度を変更することでフェードイン効果を実現
-  setTimeout(function() {
-    slides[slideIndex].style.opacity = 1;
-  }, 10);
-}
+  // 指定されたインデックスのスライドを表示する関数
+  function showSlide(slideIndex) {
+    slides.forEach(function(slide) {
+      slide.style.display = "none"; // 非表示に設定
+      slide.style.opacity = 0; // 透明度を0に設定
+    });
+    slides[slideIndex].style.display = "block"; // 指定されたスライドを表示
+    setTimeout(function() {
+      slides[slideIndex].style.opacity = 1; // フェードイン効果
+    }, 10);
+  }
 
-  // 次のスライドへ移動
   function nextSlide() {
-    // 現在のスライドインデックスを更新
-    currentSlide = (currentSlide + 1) % slides.length;
-    // 指定されたインデックスのスライドを表示
+    currentSlide = (currentSlide + 1) % slides.length; // 次のスライドへ移動
     showSlide(currentSlide);
   }
 
-  // スライドショーを開始
   function startSlideshow() {
-    // 最初のスライドを表示
     showSlide(currentSlide);
-    // 3秒ごとに次のスライドへ移動
-    setInterval(nextSlide, 3000);
+    setInterval(nextSlide, 3000); // 3秒ごとに次のスライドへ移動
   }
 
   // SP表示の場合のみスライドショーを開始
   if (window.innerWidth <= 640) {
-    // スライドショーのコンテナを表示
     document.querySelector(".sp-slideshow").style.display = "block";
-    // スライドショーを開始
     startSlideshow();
   }
 });
 
 window.addEventListener('load', function() {
-  // サイズを取得
   var width = window.innerWidth;
   var height = window.innerHeight;
-
-  // スライドショーのコンテナの要素を取得
   var slideshowContainer = document.querySelector('.sp-slideshow');
 
   if (slideshowContainer) {
-    // サイズに基づいてコンテナの高さと幅を設定
     slideshowContainer.style.width = width + 'px';
     slideshowContainer.style.height = height + 'px';
-
-    // 各スライドのサイズも同様に設定
     var slides = slideshowContainer.querySelectorAll('img');
     slides.forEach(function(slide) {
       slide.style.width = width + 'px';
-      slide.style.height = height + 'px';
+      slide.style.height = height + 'px'; // 各スライドのサイズを設定
     });
   }
 });
+
+function toggleSlideshow() {
+  if (window.innerWidth <= 641) {
+    document.querySelector('.sp-slideshow').style.display = 'block';
+    document.querySelectorAll('.sp').forEach(function(el) {
+      el.style.display = 'block';
+    });
+  } else {
+    document.querySelector('.sp-slideshow').style.display = 'none';
+    document.querySelectorAll('.sp').forEach(function(el) {
+      el.style.display = 'none';
+    });
+  }
+}
+
+// ウィンドウがリサイズされたときにも関数を実行
+window.addEventListener('resize', toggleSlideshow);
